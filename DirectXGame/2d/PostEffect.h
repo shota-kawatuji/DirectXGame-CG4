6be7b3d4e80d8b73db35217488ffca07208b2d4b
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2023-06-01 15:03:53 +0900

    3-3完了

--- DirectXGame/2d/PostEffect.h
+++ DirectXGame/2d/PostEffect.h
@@ -3,16 +3,27 @@
 class PostEffect :
     public Sprite
 {
-public:
+public: // メンバ関数
     /// <summary>
     /// コンストラクタ
     /// </summary>
     PostEffect();
 
     /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize();
+
+    /// <summary>
     /// 描画コマンドの発行
     /// </summary>
     /// <param name="cmdList">コマンドリスト</param>
     void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private: // メンバ変数
+    // テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff;
+    // SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
