d72808e5bd81213802480ae921127d71d8492b24
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2023-06-28 15:30:27 +0900

    評価課題提出

--- DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -2,7 +2,7 @@
 
 Texture2D<float4> tex0 : register(t0);  // 0番スロットに設定されたテクスチャ
 Texture2D<float4> tex1 : register(t1);  // 0番スロットに設定されたテクスチャ
-SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
+SamplerState smp : register(s0);        // 0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
