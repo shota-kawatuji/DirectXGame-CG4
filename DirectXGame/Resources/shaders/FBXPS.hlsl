496b88cc8a831ac0d2889abb34833969da6f6adb
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2023-06-28 23:53:26 +0900

    4-6完了

--- DirectXGame/Resources/shaders/FBXPS.hlsl
+++ DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -21,6 +21,35 @@ float SchlickFresnel(float f0, float f90, float cosine)
 	return lerp(f0, f90, m5);
 }
 
+// UE4のGGX分布
+// alpha : roughnessの2乗
+// BdotH : 法線とハーフベクトルの内積
+float DisributionGGX(float alpha, float NdotH)
+{
+	float alpha2 = alpha * alpha;
+	float t = NdotH * NdotH * (alpha2 - 1.0f) + 1.0f;
+	return alpha2 / (PI * t * t);
+}
+
+// 鏡面反射の計算
+float3 CookTorranceSpecular(float NdotL, float NdotV, float NdotH, float LdotH)
+{
+	// D項(分布:Distribution)
+	float Ds = DisributionGGX(roughness * roughness, NdotH);
+
+	// F項(フレネル:Fresnel)
+	float3 Fs = float3(1, 1, 1);
+
+	// G項(幾何減衰:Geometry attenuation)
+	float Gs = 0.1f;
+
+	// m項(分母)
+	float m = 4.0f * NdotL * NdotV;
+
+	// 合成して鏡面反射の色を得る
+	return Ds * Fs * Gs / m;
+}
+
 // 双方向反射分布関数
 float3 BRDF(float3 L, float3 V)
 {
@@ -55,9 +84,11 @@ float3 BRDF(float3 L, float3 V)
 	// 初期反射項
 	float3 diffuseColor = diffuseReflectance * Fd * baseColor * (1 - metalness);
 
-	//ここに鏡面反射の式を挿入する
+	// 鏡面反射項
+	float3 specularColor = CookTorranceSpecular(NdotL, NdotV, NdotH, LdotH);
 
-	return diffuseColor;
+	// 拡散反射と鏡面反射の合計で色が決まる
+	return diffuseColor + specularColor;
 }
 
 // エントリーポイント
