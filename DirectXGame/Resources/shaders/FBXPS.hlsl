5d399690ef0fc8b530b29b1175319a4f7e5b4595
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2023-06-28 22:18:00 +0900

    4-5完了

--- DirectXGame/Resources/shaders/FBXPS.hlsl
+++ DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -8,6 +8,19 @@ static const float PI = 3.141592654f;
 // 反射点の法線ベクトル
 static float3 N;
 
+// Schlickによる近似
+// f0 と f90 の値を(1 - cosine)の5乗でlerpする
+// f0 : 光が垂直に入射したときの反射率
+// f90 : 光が平行に入射したときの反射率
+// cosine : 2ベクトルのなす角のコサイン(内積値)
+float SchlickFresnel(float f0, float f90, float cosine)
+{
+	float m = saturate(1 - cosine);
+	float m2 = m * m;
+	float m5 = m2 * m2 * m;
+	return lerp(f0, f90, m5);
+}
+
 // 双方向反射分布関数
 float3 BRDF(float3 L, float3 V)
 {
@@ -18,10 +31,29 @@ float3 BRDF(float3 L, float3 V)
 	// どちらかが90度以上であれば真っ黒を返す
 	if (NdotL < 0 || NdotV < 0) { return float3(0, 0, 0); }
 
+	// ライト方向とカメラ方向の中間
+	float3 H = normalize(L + V);
+	// 法線とハーフベクトル
+	float NdotH = dot(N, H);
+	// ライトとハーフベクトルの内積
+	float LdotH = dot(L, H);
+
 	// 拡散反射率
 	float diffuseReflectance = 1.0f / PI;
+
+	float energyBias = 0.5f * roughness;
+	// 入射角が90度の場合の拡散反射率
+	float Fd90 = energyBias + 2.0f * LdotH * LdotH * roughness;
+	// 入っていく時の拡散反射率
+	float FL = SchlickFresnel(1.0f, Fd90, NdotL);
+	// 出ていく時の拡散反射率
+	float FV = SchlickFresnel(1.0f, Fd90, NdotV);
+	float energyFactor = lerp(1.0f, 1.0f / 1.51f, roughness);
+	// 入って出ていくまでの拡散反射率
+	float Fd = FL * FV * energyFactor;
+
 	// 初期反射項
-	float3 diffuseColor = diffuseReflectance * NdotL * baseColor * (1 - metalness);
+	float3 diffuseColor = diffuseReflectance * Fd * baseColor * (1 - metalness);
 
 	//ここに鏡面反射の式を挿入する
 
